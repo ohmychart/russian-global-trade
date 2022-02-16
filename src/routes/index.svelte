c9979d252f9636bf852872f558a5e7109abe9df3
d2ski <chukotski@gmail.com>
2022-02-16 21:45:47 +0200

    fix manifest path & build

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,6 +12,7 @@
 </script>
 
 <script>
+	import { base } from '$app/paths';
 	import '$styles/app.css';
 
 	export let data;
@@ -33,10 +34,10 @@
 	<meta name="description" content="" />
 	<meta name="author" content="OhMyChart.com" />
 
-	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-	<link rel="manifest" href="/site.webmanifest">
+	<link rel="apple-touch-icon" sizes="180x180" href="{base}/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="{base}/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="{base}/favicon-16x16.png">
+	<link rel="manifest" href="{base}/site.webmanifest">
 </svelte:head>
 
 <Header isMobile={$isMobileView} />
